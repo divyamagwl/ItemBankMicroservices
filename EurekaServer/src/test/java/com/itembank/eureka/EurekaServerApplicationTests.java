afa6553831cf04934615cfad0b14afabe3508ba9
Divyam <agrawaldivyam2001@gmail.com>
2022-11-20 15:58:18 +0530

    Added spring services to be implemented

---
+++ EurekaServer/src/test/java/com/itembank/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itembank.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
