4d65628037624e571757b65a719a882c2a96d861
Divyam <agrawaldivyam2001@gmail.com>
2022-11-22 16:29:14 +0530

    Added more contraints to author db

--- AuthorService/src/main/java/com/itembank/author/Controller/AuthorController.java
+++ AuthorService/src/main/java/com/itembank/author/Controller/AuthorController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
@@ -35,5 +37,12 @@ public class AuthorController {
 		Author author = repo.findByUsername(uname);
 		return new ResponseEntity<>(author, HttpStatus.OK);
 	}
+
+	@RequestMapping(path ="/id/{id}")
+	public ResponseEntity<Optional<Author>> getById(@PathVariable Integer id) {
 		
+		Optional<Author> author = repo.findById(id);
+		return new ResponseEntity<>(author, HttpStatus.OK);
+	}
+
 }
--- AuthorService/src/main/java/com/itembank/author/Entity/Author.java
+++ AuthorService/src/main/java/com/itembank/author/Entity/Author.java
@@ -10,7 +10,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Entity
 @Data
 @AllArgsConstructor
@@ -21,17 +20,21 @@ public class Author {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
-	@Column(unique=true)
+	@Column(unique=true, nullable=false)
 	private String username;
-	@Column
+
+	@Column(nullable = false)	
 	private String password;
-	@Column(unique=true)
+
+	@Column(unique=true, nullable=false)
 	private String email;
 
+	public int getId() { return id; }
     public String getUsername() { return this.username; }
     public String getPassword() { return this.password; }
     public String getEmail() { return this.email; }
 
+	public void setId(int id) { this.id = id; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
     public void setEmail(String email) { this.email = email; }
